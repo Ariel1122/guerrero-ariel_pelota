8c6f062d43f2805ae40314ea9dc50db2610a1c94
Ariel Guerrero <ariel.guerrero@ucuenca.edu.ec>
2023-09-25 16:52:05 -0500

    la pelota desaparece y cambia de colores

--- sketch.js
+++ sketch.js
@@ -1,16 +1,55 @@
+// variables
+let posX;
+let posY;
+let diametro;
+let radio;
+let colorCirculo;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  // IMPRESION DE VARIABLES
+  // print(posX);c
+  // posX = 234;
+  // print(posX);
+  // posX = posX + 25;
+  // print(posX);
+  // posX = posX * 2
+  // print(posX);
+  // posX = posX / 5;
+  // print(posX);
+  // posX *= 8;
+  // print(posX);
+  // posX += 1;
+  // print(posX);
+  // posX ++;
+  // posicion de la bolita
+  // = asigna el numero al lado debecho
+  posX = windowWidth / 2;
+  posY = windowHeight / 2;
+  diametro = 30;
+  // se automatiza el tamaño
+  radio = diametro / 2;
+  colorCirculo = color(22, 100, 50);
+  // frameRate(2); fotogramas de movimiento
 }
 
-function draw() {  
-
+// funcion de dibujado automatico
+// la pelota se mueve restando o sumando posiciones
+function draw() {
   background(255);
-  fill(mouseX);  
-  circle(windowWidth / 2, windowHeight / 2, 300);
-  textSize(20);
-  text(mouseX, 50, 50);
-  text( int(mouseY), 50, 100);
-  //rect (0, 0, mouseX, mouseY)
-  //textSize(25)
-  // text(mouseX, 100, 100);
+
+  // velocidad deternimada por el calculo de la varible
+  posX -= 5;
+  // la posicion en x del ciculo (centro) + mas 25 del tamaño general del ciculo
+  if (posX < -radio) {
+    diametro = random(10, 100);
+    colorCirculo = color(random(100, 255), random(100, 255), random(100, 255));
+    radio = diametro / 2;
+    posX = windowWidth + radio;
+    // posicion en x el ancho de la pantalla, si la posicion en x es menor a sero la volita se mueve
+  }
+  fill(colorCirculo);
+  ellipse(posX, posY, diametro, diametro);
+  // circle(posX, posY, diametro);
+  print(posX);
 }
