ac9a72068877c977e2291dfe2f77cbba098137be
Ariel Guerrero <ariel.guerrero@ucuenca.edu.ec>
2023-09-28 15:01:10 -0500

    el fondo cambia de color

--- sketch.js
+++ sketch.js
@@ -4,7 +4,9 @@ let posY;
 let diametro;
 let radio;
 let colorCirculo;
+let fondo;
 
+//EJECUCION DE LA FUNCION
 function setup() {
   createCanvas(windowWidth, windowHeight);
   // IMPRESION DE VARIABLES
@@ -14,21 +16,15 @@ function setup() {
   // posX = posX + 25;
   // print(posX);
   // posX = posX * 2
-  // print(posX);
-  // posX = posX / 5;
-  // print(posX);
-  // posX *= 8;
-  // print(posX);
-  // posX += 1;
-  // print(posX);
-  // posX ++;
+  // print(posX);;
   // posicion de la bolita
   // = asigna el numero al lado debecho
   posX = windowWidth / 2;
   posY = windowHeight / 2;
-  diametro = 30;
+  diametro = 100;
   // se automatiza el tamaño
   radio = diametro / 2;
+  fondo = color(255, 100, 50);
   colorCirculo = color(22, 100, 50);
   // frameRate(2); fotogramas de movimiento
 }
@@ -36,15 +32,17 @@ function setup() {
 // funcion de dibujado automatico
 // la pelota se mueve restando o sumando posiciones
 function draw() {
-  background(255);
+  background(fondo);
 
   // velocidad deternimada por el calculo de la varible
-  posX -= 5;
+  posX -= 10;
   // la posicion en x del ciculo (centro) + mas 25 del tamaño general del ciculo
   if (posX < -radio) {
     diametro = random(10, 100);
     colorCirculo = color(random(100, 255), random(100, 255), random(100, 255));
+    fondo = color(random(100, 255), random(100, 255), random(100, 255));
     radio = diametro / 2;
+
     posX = windowWidth + radio;
     // posicion en x el ancho de la pantalla, si la posicion en x es menor a sero la volita se mueve
   }
